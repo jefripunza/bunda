0543387977a2d1c119de377353e181e756b30375
Jefri Herdi Triyanto <jefripunza@gmail.com>
2024-07-31 02:31:56 +0700

    add unique module

---
+++ server/modules/autolayout/DESCRIPTION.md
@@ -0,0 +1,1 @@
+# Modul untuk yeaa gitu deh.
---
+++ server/modules/autolayout/index.js
@@ -0,0 +1,28 @@
+import { express } from "../../deps";
+
+// Middlewares
+import CustomRequest from "../../middlewares/custom_request.middleware";
+import UseToken from "../../middlewares/use_token.middleware";
+
+// Controllers
+
+const router = express.Router();
+
+// module
+// menu
+// role_access
+
+// layout
+// -> widget
+// -> panel
+// -> inbox
+// -> container
+// - container tab
+// - container tab button
+// - container panel
+// - container panel button
+// - container panel field
+// - container panel grid
+// - container panel widget
+
+export default router;
